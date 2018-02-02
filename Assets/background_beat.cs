ee81e6cca7cffa3f70e9310af4b4039d02a7c771
jaehosung <jaehossung@gmail.com>
2018-02-02 20:38:06 +0900

    [Make prototype]

---
+++ Assets/background_beat.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class background_beat : MonoBehaviour {
+
+
+	public AudioSource bgsound;
+	public AudioSource beatsound;
+
+	float bpm = 168;
+	int clapperbeat = 4;
+
+	float bgtime;
+	int bgbeat;
+	int prevbeat = -1;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space)){
+			bgsound.Stop();
+			bgsound.Play();
+		}
+		// Debug.Log(bgsound.time);
+		bgtime = bgsound.time;
+        bgbeat = (int)(bgtime * bpm /(60*clapperbeat));
+
+		if(bgbeat > prevbeat){
+			beatsound.Play();
+			prevbeat = bgbeat;
+			action_beat();
+		}
+    }
+	void action_beat(){
+		GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+		cube.transform.position = new Vector3(0, 0, 0);
+		cube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+	}
+}
---
+++ Assets/key_beat.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class key_beat : MonoBehaviour {
+
+	public AudioSource somesound;
+	public KeyCode key;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(key)){
+			somesound.Play();
+			GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			cube.AddComponent<Rigidbody>();
+			cube.transform.position = new Vector3(3, 0, 0);
+			cube.transform.localScale = new Vector3(0.5f, 0.5f, 0.5f);
+		}
+	}
+}
