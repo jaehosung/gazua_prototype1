c607b4d57c5973ef0c825326bd7f5d671c517e78
jaehosung <jaehossung@gmail.com>
2018-02-08 14:59:06 +0900

    Bug : First node disappear

---
+++ Assets/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+
+	public AudioSource bgSound;
+	public KeyCode key;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(key))
+		{
+			Debug.Log("what");
+			bgSound.Stop();
+			bgSound.Play();
+		}
+	}
+}
---
+++ Assets/bgManager.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bgManager : MonoBehaviour {
+	public AudioSource bgSound;
+	public AudioSource beatSound;
+	public KeyCode key;
+	public KeyCode key2;
+
+	private float bpm = 168;
+	//beat per second
+	private float bps;
+	private int beatPerBar = 4;
+
+	private float bgTime;
+	private float bgBar;
+	private float bgBeat;
+	private int prevTemp = 0;
+
+	private int sessionPerBeat = 2;
+
+	//judgement Max : 0.5 / Min : 0.1
+	private float judgement = 0.49f;
+	
+	public GameObject blueTile;
+	public GameObject blackTile;
+	public GameObject indicator;
+	private GameObject[] beatIndicators = new GameObject[8];
+	// Use this for initialization
+	void Awake(){
+		for(int i = 0; i<8; i++){
+			beatIndicators[i] = Instantiate(blueTile, new Vector3(0,0), Quaternion.identity);
+			if(i%2 == 0){
+				beatIndicators[i].transform.position = new Vector2(0.2f + 0.9f *  i/2f, 0f);
+			}else{
+				beatIndicators[i].transform.position = new Vector2(0.6f + 0.9f * (i-1)/2f,0f);
+			}
+			beatIndicators[i].SetActive(false);
+		}
+		Instantiate(indicator);
+	}
+	void Start ()
+	{
+		bps = bpm / 60;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(barEnd()){
+			foreach (GameObject obj in beatIndicators){
+				obj.SetActive(false);
+			}
+		}
+		MusicStart();
+
+		if(Input.GetKeyDown(KeyCode.Space)){
+			beatSound.Stop();
+			beatSound.Play();
+			correctBeatMaker(sessionPerBeat);
+		}
+	}
+	void correctBeatMaker(int inputPerBeat){
+		float inputBeat = bgBeat*2;
+		if(Mathf.Abs(inputBeat - Mathf.Round(inputBeat))<judgement){
+			int beat = (int)Mathf.Round(inputBeat)%8;
+			beatIndicators[beat].SetActive(true);
+		}
+	}
+
+	void indicatorMove(){
+		indicator.GetComponent<Rigidbody2D>().velocity = new Vector2(1,0);
+	}
+	void MusicStart()
+	{
+		if (Input.GetKeyDown(key))
+		{
+			bgSound.Stop();
+			bgSound.Play();
+		}
+		
+		//Calculating bgTime and bgBar and bgBeat
+		bgTime = bgSound.time;
+		bgBar = bgTime * bps / beatPerBar;
+		bgBeat = bgTime * bps;
+
+	}
+	private float barBeat = 0;
+	private int prevBeat = -1;
+
+
+	bool barEnd(){
+		if((int)bgBar != prevBeat){
+			prevBeat = (int)bgBar;
+			return true;
+		}
+		return false;
+	}
+}
