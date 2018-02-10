7defcc3520c170f8d49684a079353c3dc7adbb41
jaehosung <jaehossung@gmail.com>
2018-02-11 02:20:28 +0900

    [First Prototype] presentation

--- Assets/bgManager.cs
+++ Assets/bgManager.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class bgManager : MonoBehaviour {
 	public AudioSource bgSound;
@@ -31,17 +32,26 @@ public class bgManager : MonoBehaviour {
 	public GameObject blackTile;
 	public GameObject indicator;
 	public GameObject blackNode;
+	public GameObject blueNode;
+
+	public GameObject wrongNode;
 	private GameObject[] beatIndicators = new GameObject[8];
 	private GameObject[] beatIndicatorsNew = new GameObject[8];
 	private GameObject[] beatShoots = new GameObject[8];
 	private GameObject[] movingIndicator = new GameObject[3];
 	private GameObject[, ] downBeatArray = new GameObject[8, 14];
 	private GameObject[, ] downBeatArrayBlack = new GameObject[8, 14];
+	private GameObject[, ] downBeatArrayBlue = new GameObject[8, 14];
+	private GameObject[, ] downBeatArrayWrong = new GameObject[8, 14];
 	private List<int>[] downBoolArray = new List<int>[8];
+	//Score
+	private int score = 100;
+	public Text scoreText;
 	//# of layers the beat can get
 	private int generatingSpeed = 1;
 	//level
 	private int level = 1;
+	private bool beatTry = false;
 
 	// Use this for initialization
 	private bool[] beatChecker = new bool[8];
@@ -56,6 +66,7 @@ public class bgManager : MonoBehaviour {
 		for(int i = 0 ; i<5; i++){
 			DownArrayGenerating(1);
 		}
+		SetScore();
 
 	}
 	void Start () { }
@@ -63,51 +74,67 @@ public class bgManager : MonoBehaviour {
 	bool lastBeatCheckFlag = false;
 	void Update () {
 		MusicStart ();
+		DownBeatRenderer ();
 
-		DownBeatTailRemoving();
+		//DownBeatTailRemoving();
+
+		if (BarEnd ()) {
+			DownArrayGenerating(level);
+			// Remove Prvious Bar
+			if(beatTryChecker()){
+				lastNodeRemoving();
+			}
+
+			/*lastBeatCheckAction should be located next to beatIndicator Renderer  ==> Maybe I should add in the function*/
+			beatIndicatorRendererReset();
+			if(lastBeatCheckFlag){
+				lastBeatCheckAction();
+			}
+
+			//Beat Generating or Beat Moving
+			IndicatorMove ();
+		}
 
 		if (Input.GetKeyDown (KeyCode.Space) || Input.GetKeyDown (keyA) || Input.GetKeyDown (keyB)) {
 			beatSound.Stop ();
 			beatSound.Play ();
 			CorrectBeatMaker (sessionPerBeat);
 		}
-		if(BarEndNum(generatingSpeed)){
-			DownArrayGenerating(level);
-		}
-		if (BarEnd ()) {
-			//Beat Generating or Beat Moving
-			IndicatorMove ();
-		}
-		beatIndicatorRenderer();
-		DownBeatRenderer ();
+		SetScore();
 	}
 
 
 	/*========== Functions ========== */
-	void DownBeatTailRemoving(){
-		for(int i = 0; i <8; i++){
-			while(downBoolArray[i].Count!=0 && downBoolArray[i][downBoolArray[i].Count-1]==0){
-				downBoolArray[i].RemoveAt(downBoolArray[i].Count-1);
+	void SetScore(){
+		scoreText.text = score.ToString();
+	}
+	bool beatTryChecker(){
+		for(int i=0; i<8; i++){
+			if(beatChecker[i] == true){
+				return true;
 			}
+		}
+		return false;
+	}
+	void lastNodeRemoving(){
+		for(int i = 0; i <8; i++){
+			downBoolArray[i].RemoveAt(downBoolArray[i].Count-1);
 		}
 	}
-	void beatIndicatorRenderer(){
-		for (int i = 0; i < 8; i++) {
-			beatIndicators[i].SetActive (false);
-			for (int j = 0; j < beatChecker.Length; j++) {
-				beatChecker[j] = false;
-			}
-		}
-		if (lastBeatCheckFlag) {
+	void lastBeatCheckAction(){
 			beatIndicators[0].SetActive (true);
 			beatChecker[0] = true;
+			downBoolArrayModifier(0);
 			lastBeatCheckFlag = false;
+	}
+	void beatIndicatorRendererReset(){
+		for (int i = 0; i < 8; i++) {
+			beatIndicators[i].SetActive (false);
+			beatChecker[i] = false;
 		}
-		
 	}
 
 
-	private int[,] sampleBeatArray = new int[, ] { { 1, 0, 0, 0, 0, 0, 0, 0 }, { 1, 0, 1, 0, 0, 0, 0, 0 }, { 1, 0, 0, 0, 1, 0, 0, 0 }, { 1, 0, 1, 0, 1, 0, 0, 0 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 0, 1, 0, 1, 0 }, { 1, 1, 1, 0, 1, 0, 0, 0 }, { 1, 1, 1, 0, 1, 1, 1, 0 }, { 1, 1, 1, 0, 1, 1, 0, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 } };
 	void DownBeatRenderer () {
 		int[] temp = new int[15];
 		for (int i = 0; i < 8; i++) {
@@ -117,21 +144,38 @@ public class bgManager : MonoBehaviour {
 				if (downBoolArray[i][j] == 1) {
 					downBeatArray[i, j].SetActive (true);
 					downBeatArrayBlack[i, j].SetActive (false);
+					downBeatArrayBlue[i, j].SetActive (false);
+					downBeatArrayWrong[i, j].SetActive (false);
 				} else if(downBoolArray[i][j]==0){
 					downBeatArray[i, j].SetActive (false);
+					downBeatArrayBlack[i, j].SetActive (true);
+					downBeatArrayBlue[i, j].SetActive (false);
+					downBeatArrayWrong[i, j].SetActive (false);
+				}else if(downBoolArray[i][j]== -1){
+					downBeatArray[i, j].SetActive (false);
 					downBeatArrayBlack[i, j].SetActive (false);
+					downBeatArrayBlue[i, j].SetActive (false);
+					downBeatArrayWrong[i, j].SetActive (true);
+				}else if(downBoolArray[i][j]== 2){
+					downBeatArray[i, j].SetActive (false);
+					downBeatArrayBlack[i, j].SetActive (false);
+					downBeatArrayBlue[i, j].SetActive (true);
+					downBeatArrayWrong[i, j].SetActive (false);
 				}
 			}
 		}
 	}
+	private int[,] sampleBeatArray = new int[, ] { { 1, 0, 0, 0, 0, 0, 0, 0 }, { 1, 0, 1, 0, 0, 0, 0, 0 }, { 1, 0, 0, 0, 1, 0, 0, 0 }, { 1, 0, 1, 0, 1, 0, 0, 0 }, { 1, 0, 1, 0, 1, 0, 1, 0 }, { 1, 1, 1, 1, 1, 1, 1, 1 }, { 1, 1, 1, 0, 1, 0, 1, 0 }, { 1, 1, 1, 0, 1, 0, 0, 0 }, { 1, 1, 1, 0, 1, 1, 1, 0 }, { 1, 1, 1, 0, 1, 1, 0, 0 }, { 0, 1, 0, 1, 0, 1, 0, 1 } };
 	void DownArrayGenerating (int level) {
 		//TODO_Add code for level
 		int index = (int) Random.Range (0f, 6f);
-		Debug.Log (index);
 		for (int i = 0; i < 8; i++) {
 			int _temp = (sampleBeatArray[index,i])%2;
+			downBoolArray[i].Insert(0,_temp);
+			/* 
 			if (downBoolArray[i].Count != 0 || _temp != 0)
-				downBoolArray[i].Insert(0,_temp);
+				downBoolArray[i].Insert(0,);
+			*/
 		}
 	}
 	void DownBeatMaking () {
@@ -139,36 +183,53 @@ public class bgManager : MonoBehaviour {
 			for (int j = 0; j < 14; j++) {
 				downBeatArray[i, j] = Instantiate (blackTile, new Vector3 (0, 0), Quaternion.identity);
 				downBeatArrayBlack[i, j] = Instantiate (blackNode, new Vector3 (0, 0), Quaternion.identity);
+				downBeatArrayBlue[i, j] = Instantiate (blueNode, new Vector3 (0, 0), Quaternion.identity);
+				downBeatArrayWrong[i, j] = Instantiate (wrongNode, new Vector3 (0, 0), Quaternion.identity);
 
 
 				/*  ===== For queue ===== */
 				if(i%2 == 0){
 					downBeatArray[i,j].transform.position = new Vector2(0.2f + 0.9f *  i/2f,5.6f-j*0.4f);
 					downBeatArrayBlack[i,j].transform.position = new Vector2(0.2f + 0.9f *  i/2f,5.6f-j*0.4f);
+					downBeatArrayBlue[i,j].transform.position = new Vector2(0.2f + 0.9f *  i/2f,5.6f-j*0.4f);
+					downBeatArrayWrong[i,j].transform.position = new Vector2(0.2f + 0.9f *  i/2f,5.6f-j*0.4f);
 				}else{
 					downBeatArray[i,j].transform.position = new Vector2(0.6f + 0.9f * (i-1)/2f,5.6f-j*0.4f);
 					downBeatArrayBlack[i,j].transform.position = new Vector2(0.6f + 0.9f * (i-1)/2f,5.6f-j*0.4f);
+					downBeatArrayBlue[i,j].transform.position = new Vector2(0.6f + 0.9f * (i-1)/2f,5.6f-j*0.4f);
+					downBeatArrayWrong[i,j].transform.position = new Vector2(0.6f + 0.9f * (i-1)/2f,5.6f-j*0.4f);
 				}
 				downBeatArray[i, j].SetActive (false);
 				downBeatArrayBlack[i, j].SetActive (false);
+				downBeatArrayBlue[i, j].SetActive (false);
+				downBeatArrayWrong[i, j].SetActive (false);
 			}
 		}
 	}
 	/* ===== BEAT CHECKER ===== */
 	void CorrectBeatMaker (int inputPerBeat) {
 		float inputBeat = bgBeat * 2;
-		if (Mathf.Abs (inputBeat - Mathf.Round (inputBeat)) < judgement) {
-			int beat = (int) Mathf.Round (inputBeat) % 8;
-			/* Beat Breaker */
-			int length = downBoolArray[beat].Count;
-			if(length>0)
-				downBoolArray[beat][length-1] = 0;
-			if (beat == 0 && inputBeat < Mathf.Round (inputBeat)) {
-				lastBeatCheckFlag = true;
-			} else {
-				beatIndicators[beat].SetActive (true);
-				beatChecker[beat] = true;
-			}
+		//if (Mathf.Abs (inputBeat - Mathf.Round (inputBeat)) < judgement) {
+		int beat = ((int) Mathf.Round (inputBeat)) % 8;
+		/* Beat Breaker */
+		if (beat == 0 && inputBeat < Mathf.Round (inputBeat)) {
+			lastBeatCheckFlag = true;
+		} else {
+			lastBeatCheckFlag = false;
+			beatIndicators[beat].SetActive (true);
+			beatChecker[beat] = true;
+			downBoolArrayModifier(beat);
+		}
+	}
+	void downBoolArrayModifier(int beat){
+		if(downBoolArray[beat][downBoolArray[beat].Count-1]==1){
+			downBoolArray[beat][downBoolArray[beat].Count-1] = 2;
+			score = score + 10;
+			scoreText.color = Color.blue;
+		}else if(downBoolArray[beat][downBoolArray[beat].Count-1]==0){
+			downBoolArray[beat][downBoolArray[beat].Count-1] = -1;
+			score = score - 100;
+			scoreText.color = Color.red;
 		}
 	}
 
